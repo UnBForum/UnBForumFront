d05b9bfb39e8f5233ab12abcf35414c8373ebf7d
Lucas Boaventura <lucasxboaventura@hotmail.com>
2023-10-07 19:31:45 -0300

    Add create account form page

--- src/pages/CreateAccount/index.tsx
+++ src/pages/CreateAccount/index.tsx
@@ -1,17 +1,47 @@
+import { Logo } from '../../assets/logo'
+import { CreateAccountContainer } from './styles'
+import { UnBForumInput } from '../../components/UnBForumInput'
 import { Button } from 'native-base'
 
-import { CreateAccountContainer } from './styles'
-import { Logo } from '../../assets/logo'
+import { InputsObjectProps, inputsObject } from './inputsObject'
+import { UnBForumSelect } from '../../components/UnBForumSelect'
 
 export function CreateAccount() {
+  function renderInput(inputObject: InputsObjectProps) {
+    switch (inputObject.input) {
+      case 'select':
+        return (
+          <UnBForumSelect
+            key={inputObject.label}
+            label={inputObject.label}
+            placeholder={inputObject.placeholder}
+            options={inputObject.options}
+            accessibilityLabel={inputObject.accessibilityLabel}
+          />
+        )
+
+      default:
+        return (
+          <UnBForumInput
+            key={inputObject.label}
+            label={inputObject.label}
+            placeholder={inputObject.placeholder}
+            inputType={inputObject.type}
+            accessibilityLabel={inputObject.accessibilityLabel}
+          />
+        )
+    }
+  }
+
   return (
     <CreateAccountContainer>
       <Logo />
 
-      <div className="buttons-container">
-        <Button variant="outline" size="lg">
-          Entrar
-        </Button>
+      <div className="inputs-container">
+        {inputsObject.map((inputObject) => {
+          return renderInput(inputObject)
+        })}
+
         <Button variant="solid" size="lg">
           Criar Conta
         </Button>
--- src/pages/CreateAccount/styles.ts
+++ src/pages/CreateAccount/styles.ts
@@ -2,24 +2,26 @@ import styled from 'styled-components'
 
 export const CreateAccountContainer = styled.div`
   max-width: 65%;
+  width: 65%;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
 
   svg {
-    width: 30rem;
-    height: 25rem;
+    width: 10rem;
+    height: 12rem;
   }
 
-  .buttons-container {
+  .inputs-container {
     width: 100%;
     display: flex;
     flex-direction: column;
-    gap: 1.5rem;
+    gap: 1.4rem;
   }
 
   @media (max-width: 768px) {
-    max-width: 80%;
+    max-width: 90%;
+    width: 90%;
   }
 `
