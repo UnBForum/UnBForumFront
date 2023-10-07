84ed309173a09aa385f1dea863551c996d36b268
Lucas Boaventura <lucasxboaventura@hotmail.com>
2023-10-07 19:29:46 -0300

    Add new routes to login in application

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,20 +1,22 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
-import { History } from './pages/History'
 import { DefaultLayout } from './layouts/DefaultLayout'
-import { CreateAccount } from './pages/CreateAccount'
+import { Logon } from './pages/Logon'
+import { Login } from './pages/Login'
 import { CreateAccountLayout } from './layouts/CreateAccountLayout'
+import { CreateAccount } from './pages/CreateAccount'
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
-        <Route path="/history" element={<History />} />
-      </Route>
-      <Route path="/create-account" element={<CreateAccountLayout />}>
-        <Route path="/create-account" element={<CreateAccount />} />
         {/* <Route path="/history" element={<History />} /> */}
+      </Route>
+      <Route path="/login" element={<CreateAccountLayout />}>
+        <Route path="/login/logon" element={<Logon />} />
+        <Route path="/login/create-account" element={<CreateAccount />} />
+        <Route path="/login" element={<Login />} />
       </Route>
     </Routes>
   )
