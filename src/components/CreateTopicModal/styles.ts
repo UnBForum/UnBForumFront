032cb068afab33787056802ebe653e6a040b2b9c
Lucas Boaventura <lucasxboaventura@hotmail.com>
2023-11-19 18:59:17 -0300

    Add create topic modal component

---
+++ src/components/CreateTopicModal/styles.ts
@@ -0,0 +1,55 @@
+import { theme } from 'native-base'
+import styled from 'styled-components'
+
+export const Overlay = styled.div<{ isOpen: boolean }>`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: ${theme.colors.black + '75'}; /* fundo escurecido */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  opacity: ${({ isOpen }) => (isOpen ? '1' : '0')};
+  visibility: ${({ isOpen }) => (isOpen ? 'visible' : 'hidden')};
+  transition:
+    opacity 0.3s ease,
+    visibility 0s linear ${({ isOpen }) => (isOpen ? '0s' : '0.3s')};
+`
+
+export const Content = styled.div<{ isOpen: boolean }>`
+  background-color: white;
+  width: 60%;
+  max-width: 880px;
+  /* height: 70vh; */
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  gap: 1rem;
+
+  border-radius: 8px;
+  padding: 1.7rem 3rem;
+  box-shadow: 0 0 10px ${theme.colors.black + '50'};
+  opacity: ${({ isOpen }) => (isOpen ? '1' : '0')};
+  transform: translateY(-50px);
+  transition:
+    opacity 0.3s ease,
+    transform 0.3s ease;
+
+  & > h1 {
+    font-size: 2rem;
+    font-weight: 700;
+    line-height: 1.5;
+    color: ${theme.colors.text['700']};
+  }
+`
+
+export const ModalButtonsContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`
