a0b802d380e38ad017975440489405aefd59449d
Lucas Boaventura <lucasxboaventura@hotmail.com>
2023-11-27 23:25:20 -0300

    Add format data to create user endpoint

---
+++ src/utils/formatCreateUser.ts
@@ -0,0 +1,13 @@
+import { CreateUser, UserData } from './interfaces'
+
+export const formatCreateUser = (userData: UserData): CreateUser => {
+  const { name, email, profile, course, password } = userData
+
+  return {
+    name,
+    email,
+    password,
+    // tags: [],
+    // tags: [profile, course],
+  }
+}
