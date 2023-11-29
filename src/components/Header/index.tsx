7e62092632a950279cbbcfe0e2c6327a199e3bf3
Lucas Boaventura <lucasxboaventura@hotmail.com>
2023-11-28 23:43:07 -0300

    Improve header with responsivity and using context of application

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,24 @@
-import { theme } from 'native-base'
+import { Button, theme } from 'native-base'
 import { InlineLogo } from '../../assets/InlineLogo'
 import { ButtonUserIcon, HeaderContainer } from './styles'
 import { FaUser } from 'react-icons/fa'
 import { useNavigate } from 'react-router-dom'
 import { BaseSyntheticEvent } from 'react'
 import { useUser } from '../../hooks/user'
+import { FiLogIn } from 'react-icons/fi'
+import { useMediaQuery } from 'usehooks-ts'
 
 export function Header() {
-  const { name, token, email, scopes } = useUser()
+  const { name, token } = useUser()
   const navigate = useNavigate()
+  const isMobile = useMediaQuery('(max-width: 768px)')
 
   function handleClickUserIcon(event: BaseSyntheticEvent) {
+    event.preventDefault()
+    navigate('/profile')
+  }
+
+  function handleClickLogin(event: BaseSyntheticEvent) {
     event.preventDefault()
     navigate('/login/logon')
   }
@@ -21,11 +29,32 @@ export function Header() {
         <InlineLogo />
 
         <div className="user-container">
-          <h3 className="user-name">{name}</h3>
+          {token && (
+            <>
+              <Button
+                onPress={(e) => handleClickUserIcon(e)}
+                rightIcon={
+                  <FaUser size="30" color={theme.colors.tertiary['300']} />
+                }
+              >
+                {isMobile ? '' : name}
+              </Button>
+            </>
+          )}
 
-          <ButtonUserIcon onClick={(e) => handleClickUserIcon(e)}>
-            <FaUser size="2.5rem" color={theme.colors.tertiary['300']} />
-          </ButtonUserIcon>
+          {!token && (
+            <>
+              <Button
+                variant="solid"
+                bgColor={theme.colors.tertiary['500']}
+                _hover={{ shadow: '8' }}
+                rightIcon={<FiLogIn size={30} color={theme.colors.white} />}
+                onPress={(e) => handleClickLogin(e)}
+              >
+                {isMobile ? '' : 'Entrar'}
+              </Button>
+            </>
+          )}
         </div>
       </section>
     </HeaderContainer>
