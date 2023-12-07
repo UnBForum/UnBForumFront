8bf109cea1a04362109e26d390eb478086196b4b
Lucas Boaventura <lucasxboaventura@hotmail.com>
2023-12-07 00:27:18 -0300

    Create comment integration inside a topic page

--- src/pages/Topic/index.tsx
+++ src/pages/Topic/index.tsx
@@ -1,21 +1,26 @@
 import { useParams } from 'react-router-dom'
 import { Post } from '../../components/Post'
 import { CommentContainer, TopicContainer } from './styles'
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { getOneTopic } from '../../service/topics'
 import { Topic as TopicInterface } from '../Home'
 import { Loading } from '../../components/Loading'
+import { useUser } from '../../hooks/user'
+import { CreateComment } from '../../components/CreateComment'
+import { getTopicComments } from '../../service/comment'
 
 export function Topic() {
   const { id } = useParams()
+  const { token } = useUser()
 
   const [topic, setTopic] = useState<TopicInterface | null>(null)
 
   const [_, setIsLoadingTopic] = useState(false)
+  const [isLoadingComments, setIsLoadingComments] = useState(false)
 
   useEffect(() => window.scrollTo(0, 0), [])
+
   useEffect(() => {
-    console.log(id)
     if (id) {
       setIsLoadingTopic(true)
       getOneTopic(Number(id))
@@ -25,6 +30,20 @@ export function Topic() {
         .finally(() => setIsLoadingTopic(false))
     }
   }, [id])
+
+  const handleMakeComment = useCallback(() => {
+    if (topic) {
+      setIsLoadingComments(true)
+
+      getTopicComments(topic.id)
+        .then((response) => {
+          setTopic({ ...topic, comments: response.data })
+        })
+        .finally(() => {
+          setIsLoadingComments(false)
+        })
+    }
+  }, [topic])
 
   return (
     <TopicContainer>
@@ -37,25 +56,39 @@ export function Topic() {
             categories={topic.categories}
             content={topic.content}
             rating={topic.rating}
+            files={topic.files}
             currentRating={topic.current_user_rating}
             isInsideTopic
           />
 
           <CommentContainer>
-            {topic.comments.map((comment) => {
-              return (
-                <Post
-                  key={comment.id}
-                  id={comment.id}
-                  isComment
-                  topicId={topic.id}
-                  author={comment.author.name}
-                  rating={comment.rating}
-                  currentRating={comment.current_user_rating}
-                  content={comment.content}
-                />
-              )
-            })}
+            {token && (
+              <CreateComment
+                topicId={topic.id}
+                onCommentCreated={handleMakeComment}
+              />
+            )}
+
+            {isLoadingComments ? (
+              <Loading accessibilityLabel="Carregando os comentários do tópico..." />
+            ) : (
+              <>
+                {topic.comments.map((comment) => {
+                  return (
+                    <Post
+                      key={comment.id}
+                      id={comment.id}
+                      isComment
+                      topicId={topic.id}
+                      author={comment.author.name}
+                      rating={comment.rating}
+                      currentRating={comment.current_user_rating}
+                      content={comment.content}
+                    />
+                  )
+                })}
+              </>
+            )}
           </CommentContainer>
         </>
       ) : (
