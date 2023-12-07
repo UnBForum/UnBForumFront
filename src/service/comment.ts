a704d5d346eab5870430a8315e2605ba225421bb
Lucas Boaventura <lucasxboaventura@hotmail.com>
2023-12-07 00:27:41 -0300

    Add comments service

--- src/service/comment.ts
+++ src/service/comment.ts
@@ -1,6 +1,23 @@
 import { AxiosResponse } from 'axios'
 import api from '../api'
 
+export const createComment = async (
+  content: string,
+  topicId: number | null,
+): Promise<AxiosResponse> => {
+  const response = await api.post(`topics/${topicId}/comments/`, { content })
+
+  return response
+}
+
+export const getTopicComments = async (
+  topicId: number | null,
+): Promise<AxiosResponse> => {
+  const response = await api.get(`topics/${topicId}/comments/`)
+
+  return response
+}
+
 export const makeUpvoteComment = async (
   commentId: number,
   topicId: number | null,
