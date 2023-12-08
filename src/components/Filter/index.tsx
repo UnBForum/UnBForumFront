ec4f87169e63bf0e02011158a10dae3613592ea5
Lucas Boaventura <lucasxboaventura@hotmail.com>
2023-12-08 11:25:08 -0300

    Update FIlters style and structure

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -4,11 +4,19 @@ import { Button, SearchIcon, theme } from 'native-base'
 import Select, { MultiValue, SingleValue, StylesConfig } from 'react-select'
 import { useEffect, useState } from 'react'
 
-import { FiltersContainer, SearchContainer, OrderByContainer } from './styles'
+import {
+  FiltersContainer,
+  SearchContainer,
+  OrderByContainer,
+  OptionsContainer,
+} from './styles'
 import { unbForumSortOptions } from '../../constants'
 import { Category } from '../../utils/interfaces'
 import { getAllCategories } from '../../service/categories'
-import { SearchInput } from '../../pages/Home/styles.ts'
+import { UnBForumInput } from '../UnBForumInput/index.tsx'
+import { FaFilter } from 'react-icons/fa'
+import { PiArrowsDownUpBold } from 'react-icons/pi'
+import { useMediaQuery } from 'usehooks-ts'
 
 interface OptionsType {
   value: string
@@ -23,76 +31,81 @@ interface FilterProps {
   handleSearch: () => void
 }
 
-const colourStyles: StylesConfig<OptionsType> = {
-  control: (styles) => ({ ...styles, backgroundColor: theme.colors.white }),
-  option: (styles, { data, isMulti, isDisabled, isFocused, isSelected }) => {
-    const color = chroma(data.color)
-    return {
-      ...styles,
-      backgroundColor: isDisabled
-        ? undefined
-        : isSelected
-          ? isMulti
-            ? data.color
-            : color.alpha(0.2).css()
-          : isFocused
-            ? color.alpha(0.1).css()
-            : undefined,
-      color: isDisabled
-        ? '#ccc'
-        : isSelected
-          ? isMulti
-            ? chroma.contrast(color, 'white') > 2
-              ? 'white'
-              : 'black'
-            : data.color
-          : data.color,
-      cursor: isDisabled ? 'not-allowed' : 'default',
-
-      ':active': {
-        ...styles[':active'],
-        backgroundColor: !isDisabled
-          ? isSelected
-            ? data.color
-            : color.alpha(0.3).css()
-          : undefined,
-      },
-    }
-  },
-  multiValue: (styles, { data }) => {
-    const color = chroma(data.color)
-    return {
-      ...styles,
-      backgroundColor: color.alpha(0.1).css(),
-    }
-  },
-  multiValueLabel: (styles, { data }) => ({
-    ...styles,
-    color: data.color,
-  }),
-  multiValueRemove: (styles, { data }) => ({
-    ...styles,
-    color: data.color,
-    ':hover': {
-      backgroundColor: data.color,
-      color: 'white',
-    },
-  }),
-  input: (styles) => ({
-    ...styles,
-    'input:focus': {
-      boxShadow: 'none',
-    },
-  }),
-}
-
 export function Filter({
   onChangeCategoriesFilter,
   onChangeOrderBy,
   onChangeSearchInput,
   handleSearch,
 }: FilterProps) {
+  const isMobile = useMediaQuery('(max-width: 768px)')
+
   const [categories, setCategories] = useState<Category[]>([])
+
+  const colourStyles: StylesConfig<OptionsType> = {
+    control: (styles) => ({
+      ...styles,
+      backgroundColor: theme.colors.white,
+    }),
+    option: (styles, { data, isMulti, isDisabled, isFocused, isSelected }) => {
+      const color = chroma(data.color)
+      return {
+        ...styles,
+        backgroundColor: isDisabled
+          ? undefined
+          : isSelected
+            ? isMulti
+              ? data.color
+              : color.alpha(0.2).css()
+            : isFocused
+              ? color.alpha(0.1).css()
+              : undefined,
+        color: isDisabled
+          ? '#ccc'
+          : isSelected
+            ? isMulti
+              ? chroma.contrast(color, 'white') > 2
+                ? 'white'
+                : 'black'
+              : data.color
+            : data.color,
+        cursor: isDisabled ? 'not-allowed' : 'default',
+
+        ':active': {
+          ...styles[':active'],
+          backgroundColor: !isDisabled
+            ? isSelected
+              ? data.color
+              : color.alpha(0.3).css()
+            : undefined,
+        },
+      }
+    },
+    multiValue: (styles, { data }) => {
+      const color = chroma(data.color)
+      return {
+        ...styles,
+        backgroundColor: color.alpha(0.1).css(),
+      }
+    },
+    multiValueLabel: (styles, { data }) => ({
+      ...styles,
+      color: data.color,
+    }),
+    multiValueRemove: (styles, { data }) => ({
+      ...styles,
+      color: data.color,
+      ':hover': {
+        backgroundColor: data.color,
+        color: 'white',
+      },
+    }),
+    input: (styles) => ({
+      ...styles,
+      'input:focus': {
+        boxShadow: 'none',
+      },
+    }),
+  }
 
   useEffect(() => {
     getAllCategories().then((response) => {
@@ -101,50 +114,63 @@ export function Filter({
   }, [])
 
   return (
-    <>
-      <Select
-        isMulti
-        closeMenuOnSelect={false}
-        options={categories.map(
-          (category): OptionsType => ({
-            value: category.id.toString(),
-            label: category.name,
-            color: category.color,
-          }),
-        )}
-        styles={colourStyles}
-        onChange={onChangeCategoriesFilter}
-        placeholder="Filtrar por categorias"
-      />
-
+    <OptionsContainer>
       <FiltersContainer>
-        <SearchContainer>
-          <SearchInput
-            name="topic-search"
-            size="xl"
-            accessibilityLabel="Buscar tópicos"
-            placeholder="Buscar tópicos..."
-            inputType="text"
-            onChange={onChangeSearchInput}
-            // @ts-expect-error InputRightElement
-            InputRightElement={
-              <Button rounded="none" w="1/12" h="full" onPress={handleSearch}>
-                <SearchIcon color={theme.colors.white} size="22" />
-              </Button>
-            }
-          />
-        </SearchContainer>
-        <OrderByContainer>
-          <Select
-            isMulti={false}
-            defaultValue={unbForumSortOptions[0]}
-            options={unbForumSortOptions}
-            placeholder="Ordenar Tópicos"
-            styles={colourStyles}
-            onChange={onChangeOrderBy}
-          />
-        </OrderByContainer>
+        <FaFilter size={20} color={theme.colors.tertiary['300']} />
+
+        <Select
+          isMulti
+          closeMenuOnSelect={false}
+          options={categories.map(
+            (category): OptionsType => ({
+              value: category.id.toString(),
+              label: category.name,
+              color: category.color,
+            }),
+          )}
+          styles={colourStyles}
+          onChange={onChangeCategoriesFilter}
+          placeholder="Filtrar por categorias"
+        />
       </FiltersContainer>
-    </>
+
+      <OrderByContainer>
+        <PiArrowsDownUpBold size={22} color={theme.colors.tertiary['300']} />
+
+        <Select
+          isMulti={false}
+          defaultValue={unbForumSortOptions[0]}
+          options={unbForumSortOptions}
+          placeholder="Ordenar Tópicos"
+          styles={colourStyles}
+          onChange={onChangeOrderBy}
+        />
+      </OrderByContainer>
+
+      <SearchContainer>
+        <SearchIcon color={theme.colors.tertiary['300']} size="22" />
+
+        <UnBForumInput
+          name="topic-search"
+          height="2.4rem"
+          // size={isMobile ? 'md' : 'lg'}
+          backgroundColor={theme.colors.white}
+          accessibilityLabel="Buscar tópicos"
+          placeholder="Buscar tópicos..."
+          inputType="text"
+          onChange={onChangeSearchInput}
+          // @ts-expect-error InputRightElement
+          InputRightElement={
+            <Button rounded="none" w="1/12" h="full" onPress={handleSearch}>
+              <SearchIcon color={theme.colors.white} size="22" />
+            </Button>
+          }
+        />
+      </SearchContainer>
+
+      {/* <FiltersContainer>
+        <SearchContainer></SearchContainer>
+      </FiltersContainer> */}
+    </OptionsContainer>
   )
 }
--- src/components/Filter/styles.ts
+++ src/components/Filter/styles.ts
@@ -1,33 +1,73 @@
 import { theme } from 'native-base'
 import styled from 'styled-components'
 
-export const FiltersContainer = styled.div`
+export const OptionsContainer = styled.div`
   width: 100%;
+  background-color: ${theme.colors.info['700']};
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  flex-direction: column;
+  padding: 0.7rem 0.7rem;
   border-radius: 5px;
   gap: 1rem;
+
   color: white;
 
   @media (max-width: 768px) {
     flex-direction: column;
+    gap: 0.5rem;
+  }
+`
+
+export const FiltersContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  gap: 1rem;
+
+  & > div {
+    width: 95%;
+  }
+
+  @media (max-width: 768px) {
+    gap: 0.5rem;
   }
 `
 
 export const SearchContainer = styled.div`
-  width: 60%;
-  background-color: ${theme.colors.white};
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  gap: 1rem;
+
+  & > div {
+    width: 95%;
+  }
 
   @media (max-width: 768px) {
-    width: 100%;
+    /* width: 100%; */
+    gap: 0.5rem;
   }
 `
 
 export const OrderByContainer = styled.div`
-  width: 40%;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  gap: 1rem;
+
+  & > div {
+    width: 95%;
+  }
 
   @media (max-width: 768px) {
-    width: 100%;
+    /* width: 100%; */
+    gap: 0.5rem;
   }
 `
