97a8e83ece259e682f54ef96fcd8ac64272247e7
Lucas Boaventura <lucasxboaventura@hotmail.com>
2023-12-09 12:40:00 -0300

    Add create and update category service

--- src/service/categories.ts
+++ src/service/categories.ts
@@ -1,6 +1,31 @@
 import { AxiosResponse } from 'axios'
 import api from '../api'
+import { Category } from '../utils/interfaces'
 
 export const getAllCategories = async (): Promise<AxiosResponse> => {
   return await api.get('categories/')
+}
+
+export const createCategory = async (
+  categoryData: Category,
+): Promise<AxiosResponse> => {
+  const response = await api.post('categories/', categoryData)
+
+  return response
+}
+
+export const deleteCategory = async (id: number): Promise<AxiosResponse> => {
+  const response = await api.delete(`categories/${id}/`)
+
+  return response
+}
+
+export const updateCategory = async (
+  categoryData: Category,
+): Promise<AxiosResponse> => {
+  const response = await api.patch(`categories/${categoryData.id}/`, {
+    ...categoryData,
+  })
+
+  return response
 }
